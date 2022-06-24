26a081ae62d5bc9b3987c5b02deb7f2144b63ce0
Braifz <braian94lp@gmail.com>
2022-06-24 11:08:14 -0300

    vowelCounter problem

---
+++ vowelCounter.js
@@ -0,0 +1,22 @@
+let str = "hOla";
+let str1 = "aerEauiOs";
+
+const vowelCounter = (str) => {
+  let num = 0;
+  let strToLowerCase = str.toLowerCase();
+  for (let i = 0; i <= strToLowerCase.length; i++) {
+    if (
+      strToLowerCase[i] == "a" ||
+      strToLowerCase[i] == "e" ||
+      strToLowerCase[i] == "i" ||
+      strToLowerCase[i] == "o" ||
+      strToLowerCase[i] == "u"
+    ) {
+      num++;
+    }
+  }
+  return num;
+};
+
+const r = vowelCounter(str1);
+console.log(r);
