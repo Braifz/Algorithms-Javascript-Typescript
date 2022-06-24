29c316f016c37f22af00557c0a88efb152697822
Braifz <braian94lp@gmail.com>
2022-06-24 11:07:38 -0300

    add another solution

--- binarySearch.js
+++ binarySearch.js
@@ -1,7 +1,7 @@
-const arr = [-1, 0, 3, 5, 9, 12]; // target = 9  // output expected = 4  // 9 exists in nums and its index is 4
+const arr = [-1, 0, 3, 5, 9, 12, 15, 19, 24, 37, 42, 51, 55, 61]; // target = 9  // output expected = 4  // 9 exists in nums and its index is 4
 
 // this isn't a binary search
-const binarySearch = (nums, target) => {
+const notBinarySearch = (nums, target) => {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === target) {
       return i;
@@ -11,7 +11,7 @@ const binarySearch = (nums, target) => {
 };
 
 // this isn't a binary search
-const binarySearch2 = (nums, target) => {
+const notBinarySearch2 = (nums, target) => {
   const output = nums.filter((e, i) => {
     if (e === target) {
       return i;
@@ -38,8 +38,33 @@ const realBinarySearch = (arr, target) => {
   return false;
 };
 
-const r2 = realBinarySearch(arr, 12);
-console.log(r2);
+// return the number of interations
+const binarySearch = (arr, numToFind) => {
+  let start = 0;
+  let end = arr.length - 1;
+  let num = 0;
+
+  while (start <= end) {
+    num++;
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] === numToFind) {
+      return num;
+    } else if (numToFind > arr[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return num;
+};
+
+let arrTest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arrTest1 = [2, 5, 8, 12, 23, 38, 56, 72, 91];
+const r3 = binarySearch(arrTest1, 23);
+console.log(r3);
+
+// const r2 = realBinarySearch(arr, 61);
+// console.log(r2);
 
 // const r = binarySearch(arr, 9);
 // console.log(r);
